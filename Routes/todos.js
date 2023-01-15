66bfa7e34e1b252b77034bf0e1975b754b0bccd8
Ladeed <muhammed.ladeed123@gmail.com>
2023-01-15 12:00:48 +0530

    Changes authorizer var name

--- Routes/todos.js
+++ Routes/todos.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const { createTodo, deleteTodo, updateTodo, getTodo, getAllTodo, updateTodoListStatus } = require('../Controllers/todosController')
 
-const authorizer = require("../middleware/authorizer")
+const userAuthorizer = require("../middleware/userAuthorizer")
 const router = express.Router()
 
-router.post(`/todo`, authorizer, createTodo)
-router.delete(`/todo/:id`, authorizer, deleteTodo)
+router.post(`/todo`, userAuthorizer, createTodo)
+router.delete(`/todo/:id`, userAuthorizer, deleteTodo)
 router.get(`/todo/:id`, getTodo)
-router.get(`/todo`, authorizer, getAllTodo)
-router.put(`/todo/:id`, authorizer, updateTodo)
-router.put(`/updateTodoListStatus/:id`, authorizer, updateTodoListStatus)
+router.get(`/todo`, userAuthorizer, getAllTodo)
+router.put(`/todo/:id`, userAuthorizer, updateTodo)
+router.put(`/updateTodoListStatus/:id`, userAuthorizer, updateTodoListStatus)
 module.exports = router
